45b0cd64d979de6e977f96bc1d9d9b15a55bf0b5
panquequelol <rene@outlook.my>
2023-10-02 17:27:26 -0300

    fix: remove unused vars

--- src/lib/Carousel.tsx
+++ src/lib/Carousel.tsx
@@ -2,7 +2,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
-import { ReactNode } from "react";
 
 type Props = {
   images: string[] | { image: string; alt: string }[];
