80ddadc6a5f13dd29a932891024ec455c15cdf69
panquequelol <rene@outlook.my>
2023-10-05 13:44:25 -0300

    Revert "feat: add sheet"
    This reverts commit 6e0ca652832ba8cc5e422e459bccb9c9d0f506f6.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,7 @@
 // WARNING
 // if there's code in here, it will be bundle for the library
 
-// export { Carousel } from "./Carousel";
 export { cn } from "./utils";
+// export { Carousel } from "./Carousel";
 export { Drawer } from "./Drawer";
 export { Button } from "./../components/ui/button";
-export * from "@/components/ui/sheet";
