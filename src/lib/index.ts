6e0ca652832ba8cc5e422e459bccb9c9d0f506f6
panquequelol <rene@outlook.my>
2023-10-05 13:40:02 -0300

    feat: add sheet

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,8 @@
 // WARNING
 // if there's code in here, it will be bundle for the library
 
-export { cn } from "./utils";
 // export { Carousel } from "./Carousel";
+export { cn } from "./utils";
 export { Drawer } from "./Drawer";
 export { Button } from "./../components/ui/button";
+export * from "@/components/ui/sheet";
